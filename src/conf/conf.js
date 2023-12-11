0500ad53a51bd8d876ae42329c312e6d8e338df6
marksikaundi <emmanuelmark117@gmail.com>
2023-12-11 12:20:47 +0200

    Refactor function to improve performance
    Add error handling for null values
    
    Update variable names for clarity
    
    Fix bug in conditional statement
    
    Update comments for better documentation
    
    Remove unused imports
    
    Optimize database query
    
    Update README with new instructions
    
    Fix formatting issues
    
    Add unit tests for new feature

---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const conf = {
+    appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+
+}
+
+
+export default conf
